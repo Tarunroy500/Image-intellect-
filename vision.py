a34be25ee2888f46963414112f08d02a8b71d9fa
Tarun Roy <tarunroy500@gmail.com>
2025-03-05 23:33:28 +0530

    Update image display to use_container_width for better layout

--- vision.py
+++ vision.py
@@ -54,7 +54,7 @@ image=""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_container_width=True)
 
 
 submit=st.button("Tell me about the image")
